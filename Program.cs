cb0f22fa8eabb600a41bb6d8d178bb2aaee4078b
zearce <zearcehuang@gmail.com>
2017-02-24 10:41:40 +0800

    第一版

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Beep");
+            Console.Beep(1000,1000);
+        }
+    }
+}
